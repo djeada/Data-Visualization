0100aed01cb69831dcf5ce5e6045b15bc974c75b
djeada <addjellouli1@gmail.com>
2022-11-02 12:32:36 +0100

    [feature]: add examples with maps

--- examples/matplotlib/scripts/sine_animation.py
+++ examples/matplotlib/scripts/sine_animation.py
@@ -5,15 +5,17 @@ import matplotlib.animation as animation
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-x = np.linspace(0, 2*np.pi, 100)
+x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
 
-graph, = ax.plot([], [])
-marker, = ax.plot([], [], color='lightblue')
+(graph,) = ax.plot([], [])
+(marker,) = ax.plot([], [], color="lightblue")
+
 
 def update_sine(i):
-    graph.set_data(x[:i],y[:i])
-    marker.set_data(x[i],y[i])
+    graph.set_data(x[:i], y[:i])
+    marker.set_data(x[i], y[i])
+
 
 anim = animation.FuncAnimation(fig, update_sine, frames=len(x), interval=50)
 plt.show()
